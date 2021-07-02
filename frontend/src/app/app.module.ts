9ddd2a6ad02cada1db19fbbd45547e8682a6db9a
Matheus.Gondim <matheus.gondim@ceresinvestimentos.com>
2021-07-01 23:51:58 -0300

    Adicionando os import MatSidenavModul e MatListModul dentro AppModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
@@ -15,7 +18,9 @@ import { NavComponent } from './components/template/nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
