0c3238f4add68c3f91959b8c35c7abd776bcdd20
Matheus.Gondim <matheus.gondim@ceresinvestimentos.com>
2021-07-03 16:38:03 -0300

    Adicionado o cliente http e implementado a função para salvar um produto no banco de dados

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +33,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,11 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { Product } from './product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  constructor(private snackBar: MatSnackBar) {}
+  private readonly BASE_URL: string = 'http://localhost:3001/products';
+
+  constructor(
+    private readonly snackBar: MatSnackBar,
+    private readonly http: HttpClient
+  ) {}
 
   showMessage(msg: string) {
     this.snackBar.open(msg, 'X', {
@@ -13,5 +21,9 @@ export class ProductService {
       horizontalPosition: 'right',
       verticalPosition: 'top'
     });
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.BASE_URL, product);
   }
 }
